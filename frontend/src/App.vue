7195249da857fa24ef46b4f6481819edcdfad862
seryte <382643558@qq.com>
2020-06-19 23:18:17 +0800

    规范vue工程

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import index from "./router/";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: index,
 }
 </script>
 
