ff07cb66a326fe77cb8071136bedb30320eaf2c4
seryte <382643558@qq.com>
2020-07-06 23:04:45 +0800

    https://bootstrap-vue.org/docs

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>
+      <button class="btn btn-primary">确定</button><br>
+      <button class="btn btn-success">使用</button><br>
+      <button class="btn btn-danger">注意</button><br>
+    </h1>
   </div>
 </template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,88 +1,48 @@
+<!-- 首页 -->
 <template>
-    <div class="headBox">
-        <el-menu
-                :default-active="activeIndex2"
-                class="el-menu-demo"
-                mode="horizontal"
-                @select="handleSelect"
-                background-color="#545c64"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-            <el-menu-item index="1">博客首页</el-menu-item>
-            <el-submenu index="2">
-                <template slot="title">分类</template>
-                <el-menu-item index="2-1">Python</el-menu-item>
-                <el-menu-item index="2-2">Javascript</el-menu-item>
-                <el-menu-item index="2-3">Go</el-menu-item>
-                <el-submenu index="2-4">
-                    <template slot="title">科学上网</template>
-                    <el-menu-item index="2-4-1">选项1</el-menu-item>
-                    <el-menu-item index="2-4-2">选项2</el-menu-item>
-                    <el-menu-item index="2-4-3">选项3</el-menu-item>
-                </el-submenu>
-            </el-submenu>
-            <el-menu-item index="3">留言板</el-menu-item>
-            <el-menu-item index="4"><a href="javascript:void(0)">关于我</a></el-menu-item>
-
-                <el-menu-item index="5"><a href="javascript:void(0);" @click="logoinFun(1)">登录&nbsp;</a>|<a
-                        href="javascript:void(0);"
-                        @click="logoinFun(0)">&nbsp;注册</a>
-                </el-menu-item>
-                <el-menu-item class="pcsearchinput" :class="input?'hasSearched':''">
-                    <el-input placeholder="搜索" icon="search" v-model="input" :on-icon-click="searchEnterFun"
-                              @keyup.enter.native="searchEnterFun" @change="searchChangeFun">
-                    </el-input>
-                </el-menu-item>
-
-
-        </el-menu>
-        <el-container>
-  <el-header>Header</el-header>
-  <el-main>Main</el-main>
-  <el-footer>Footer</el-footer>
-</el-container>
+    <div>
+        <wbc-nav></wbc-nav>
+        <div class="container">
+            <el-row  :gutter="30">
+                <el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+<!--                    <wbc-sharelist></wbc-sharelist>-->
+                </el-col>
+                <el-col :sm="24"  :md="8" >
+<!--                    <wbc-rightlist></wbc-rightlist>-->
+                </el-col>
+            </el-row>
+        </div>
+        <wbc-footer></wbc-footer>
     </div>
-
 </template>
 
 <script>
-    // // @ is an alias to /src
-    // import HelloWorld from '@/components/HelloWorld.vue'
-    //
-    // export default {
-    //   name: 'Home',
-    //   components: {
-    //     HelloWorld
-    //   }
-    // }
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+// import temSharelist from '../components/temSharelist.vue'
+// import temRightlist from '../components/temRightlist.vue'
     export default {
-        data() {
+        name:'Home',
+        data() { //选项 / 数据
             return {
-                activeIndex: '1',
-                activeIndex2: '1'
-            };
-        },
-        methods: {
-            handleSelect(key, keyPath) {
-                console.log(key, keyPath);
+
             }
+        },
+        methods: { //事件处理器
+
+        },
+        components: { //定义组件
+            'wbc-nav':Header,
+            // 'wbc-sharelist':temSharelist,
+            // 'wbc-rightlist':temRightlist,
+            'wbc-footer':Footer
+        },
+        created() { //生命周期函数
+
         }
     }
 </script>
-<style scoped>
-    .headBox li.is-active {
-        background: #48456C;
-        background: rgba(73, 69, 107, 0.7);
-    }
-    .headBox .pcsearchbox {
-	padding: 0;
-	max-width: 170px;
-	/*min-width: 30px;*/
-	height: 100%;
-	line-height: 38px;
-	position: absolute;
-	top: 0;
-	right: 0;
-	cursor: pointer;
-}
+
+<style>
+
 </style>
