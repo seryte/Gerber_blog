a2e8b9d3e860d880988fc3e7a56fc9804ec0e542
seryte <382643558@qq.com>
2020-08-19 23:50:52 +0800

    本地nginx解决中跨域问题，注册用户接口开发中

--- backend/blog_interface/user_views/views.py
+++ backend/blog_interface/user_views/views.py
@@ -1,8 +1,16 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 
 
-def joint_debugging(request):
-    return HttpResponse("注册mock!")
+def register_user(request):
+    body = request.body
+    data = json.loads(body, encoding='utf-8')
+    print(data)
+    # user_name = request.get("username", "")
+    # user_email = request.get()
+    return HttpResponse(f"注册mock!{data}")
